c87ed32420f8d2192408d600a3780f2976416ce6
Mergulhao <ewtm@msn.com>
2020-08-28 09:10:09 -0300

    inicio do app agenda

---
+++ src/app/contato.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Contato } from './contato/contato';
+import { Observable } from 'rxjs';
+
+import { environment } from '../environments/environment';
+
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ContatoService {
+
+  url: string = environment.apiBaseurl;
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+
+   save(contato: Contato): Observable<Contato>{
+     return this.http.post<Contato>(this.url,contato);
+
+   } 
+
+}
